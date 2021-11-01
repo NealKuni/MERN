32f04a694c0b87ae297ea8b15df76496c2bef773
Neal Ichinohe <neal.kuni@yahoo.com>
2021-11-01 13:54:27 -0600

    first commit

---
+++ my_app/src/components/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+
+const Profile = () => {
+    return (
+        <div>
+            <h1>Hello!</h1>
+            <p>Neal!</p>
+        </div>
+    )
+}
+
+export default Profile;
