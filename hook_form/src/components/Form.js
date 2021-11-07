5a1614948766b250c5b73b665414e8a2e8defe72
Neal Ichinohe <neal.kuni@yahoo.com>
2021-11-06 22:26:53 -0600

    Added validations for the "More Forms" assignment

--- hook_form/src/components/Form.js
+++ hook_form/src/components/Form.js
@@ -6,6 +6,12 @@ const Form = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
+    const [firstNameError, setFirstNameError] = useState("");
+    const [lastNameError, setLastNameError] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [confirmPasswordError, setConfirmPasswordError] = useState("");
+
 
     const createUser = (e) => {
         e.preventdefault();
@@ -18,28 +24,94 @@ const Form = () => {
         };
         console.log(newUser);
     };
+    const handleFirstName = (e) =>{
+        setFirstName(e.target.value);
+        if(e.target.value.length < 3) {
+            setFirstNameError('First name requires at least 2 characters.');
+        } else {
+            setFirstNameError("");
+        }
+    };
+    const handleLastName = (e) =>{
+        setLastName(e.target.value);
+        if(e.target.value.length < 3) {
+            setLastNameError('Last name requires at least 2 characters.');
+        } else {
+            setLastNameError("");
+        }
+    };
+    const handleEmail = (e) =>{
+        setEmail(e.target.value);
+        if(e.target.value.length < 3) {
+            setEmailError('Email requires at least 2 characters.');
+        } else {
+            setEmailError("");
+        }
+    };
+    const handlePassword = (e) =>{
+        setPassword(e.target.value);
+        if(e.target.value.length < 9) {
+            setPasswordError('Password requires at least 8 characters.');
+        } else {
+            setPasswordError("");
+        }
+    };
+    const handleConfirmPassword = (e) =>{
+        setConfirmPassword(e.target.value);
+        if(e.target.value !== password) {
+            setConfirmPasswordError('Passwords must match.')
+        } else {
+            setConfirmPasswordError("")
+        }
+    };
+
     return (
         <div>
             <form onSubmit={ createUser }>
                 <div>
                     <label> First Name:</label>
-                    <input type="text" onChange={ (e) => setFirstName(e.target.value)} />
+                    <input type="text" onChange={ handleFirstName } />
+                    {
+                        firstNameError ?
+                        <p>{firstNameError}</p>:
+                        ""
+                    }
                 </div>
                 <div>
                     <label> Last Name:</label>
-                    <input type="text" onChange={ (e) => setLastName(e.target.value)} />
+                    <input type="text" onChange={ handleLastName } />
+                    {
+                        lastNameError ?
+                        <p>{lastNameError}</p>:
+                        ""
+                    }
                 </div>
                 <div>
                     <label> Email Address:</label>
-                    <input type="text" onChange={ (e) => setEmail(e.target.value)} />
+                    <input type="text" onChange={ handleEmail } />
+                    {
+                        emailError ?
+                        <p>{emailError}</p>:
+                        ""
+                    }
                 </div>
                 <div>
                     <label> Password:</label>
-                    <input type="text" onChange={ (e) => setPassword(e.target.value)} />
+                    <input type="password" onChange={ handlePassword } />
+                    {
+                        passwordError ?
+                        <p>{passwordError}</p>:
+                        ""
+                    }
                 </div>
                 <div>
                     <label> Confrim Password:</label>
-                    <input type="text" onChange={ (e) => setConfirmPassword(e.target.value)} />
+                    <input type="password" onChange={ handleConfirmPassword } />
+                    {
+                        confirmPasswordError ?
+                        <p>{confirmPasswordError}</p>:
+                        ""
+                    }
                 </div>
                 <input type="submit" value="Create User" />
             </form>
