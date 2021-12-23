67e47c6bb1fd6174ee978861d2bc21775539b443
Neal Ichinohe <neal.kuni@yahoo.com>
2021-12-22 23:17:19 -0700

    Chaining methods

--- OOP/User.js
+++ OOP/User.js
@@ -5,36 +5,32 @@ class User {
         this.accountBalance = 0
     }
     makeDeposit(amount) {                // takes a parameter this is the amount of the deposit
-        this.accountBalance += amount;   // the specific user's account increases by the amount of the value received
-    }
+      this.accountBalance += amount;   // the specific user's account increases by the amount of the value received
+      return this;
+    }
     makeWithdrawal(amount) {
       this.accountBalance -= amount;
+      return this;
     }
     displayBalance(){
       console.log(this.accountBalance);
+      return this;
     }
     transferMoney(amount, user){
       this.accountBalance -= amount;
       user.accountBalance += amount;
+      return this;
     }
 }
+
 const neal = new User('neal','neal@dojo.com');
 const alex = new User('alex', 'alex@dojo.com');
 const cleo = new User('cloe', 'cleo@dojo.com');
 
-neal.makeDeposit(100);
-neal.makeDeposit(100);
-neal.makeDeposit(100);
+neal.makeDeposit(100).makeDeposit(100).makeDeposit(100).displayBalance();
 
-alex.makeDeposit(2000);
-alex.makeDeposit(2000);
-alex.makeWithdrawal(1000);
-alex.makeWithdrawal(1000);
+alex.makeDeposit(2000).makeDeposit(2000).makeWithdrawal(1000).makeWithdrawal(1000).displayBalance();
 
-neal.displayBalance();
-alex.displayBalance();
-
-neal.transferMoney(100, alex);
-neal.displayBalance();
+neal.transferMoney(100, alex).displayBalance();
 alex.displayBalance();
 
